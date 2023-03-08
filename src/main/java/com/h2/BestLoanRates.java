2ecb8fdf8d2f1d090491ebbe19cfd067b3386dcb
LeonZoet <leon.zoet@ing.com>
2023-03-08 16:00:54 +0100

    Completed module

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -17,20 +17,20 @@ public class BestLoanRates {
             System.out.println("No available rates for term : " + loanTermInYears + "years");
         }
         else {
-            System.out.println("Best available Rate : " + getRates(loanTermInYears) + "%");
+            System.out.println("Best Available Rate : " + getRates(loanTermInYears) + "%");
         }
         scanner.close();
     }
 
-    private static final Map<Integer, Float> bestRates
+    public static final Map<Integer, Float> bestRates
             = Map.of(1, 5.50f,
             2, 3.45f,
-            3, 267f);
+            3, 2.67f);
 
     public static float getRates(int loanTermInYears) {
         if (bestRates.containsKey(loanTermInYears)) {
             return bestRates.get(loanTermInYears);
-        } else
+        }
            return 0.0f;
 
     }
